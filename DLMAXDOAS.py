be516565729b939fb830fb3e9bd4baa80960669a
Zhichao Pan <charles.zhpan@gmail.com>
2022-02-12 18:39:06 -0500

    2022/02/12 Update

--- DLMAXDOAS.py
+++ DLMAXDOAS.py
@@ -1,80 +1,102 @@
 import os
 import sys
+import numpy as np
+import pandas as pd
 import random
 import warnings
 import glob
 import netCDF4 as nc
-
-import numpy as np
-import pandas as pd
-
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import Model, load_model, Sequential
-from keras.layers import Input, LSTM, Dense, Permute, Reshape, LeakyReLU
-from keras.layers.core import Lambda
-from keras.layers.convolutional import Conv1D, SeparableConv2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.merge import concatenate
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.utils import normalize
-from keras.regularizers import l2
-import keras.layers as kl
-from keras.utils import Sequence
-
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras import backend as K
-from keras.optimizers import SGD, Adam
-from keras.metrics import mean_squared_error
 import tensorflow as tf
-from keras.callbacks import CSVLogger
+from tensorflow import keras
+
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import Model, load_model, Sequential
+from tensorflow.keras.layers import Input, LSTM, Dense, Permute, Reshape, LeakyReLU, Lambda
+from tensorflow.keras.layers import Conv1D, SeparableConv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.utils import normalize
+from tensorflow.keras.regularizers import l2
+import tensorflow.keras.layers as kl
+from tensorflow.keras.utils import Sequence
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import backend as K
+from tensorflow.keras.metrics import mean_squared_error
+from tensorflow.keras.callbacks import CSVLogger
 
 from numpy.random import seed, randint, rand, uniform
+
+## Helper functions and constant variables
+
+INPATH = "../input/pandora/data/"
+OUTPATH = "../working/"
+SPLIT = [0.7, 0.85]  # Splitting ratio 0.70-0.15-0.15
+BCHSIZE = 200  # Batch size
+EPOCH = 500  # Epochs
+LR = 1e-6  # Learning rate
+AVK = True  # Include averaging kernels as input
+FLAG = 2
+
+
+# Choose threshold combination for data filtering
+# 1 - dof > 1.5 & chisq < 40 (best)
+# 2 - dof < 1.5 & chisq > 40 (worst)
+# 3 - dof > 1.5 & chisq > 40
+# 4 - dof < 1.5 & chisq < 40
 
 
 def r2_keras(y_true, y_pred):
     y_t = tf.multiply(y_true, tf.cast(tf.not_equal(y_true, 0), tf.float32))
     y_p = tf.multiply(y_pred, tf.cast(tf.not_equal(y_true, 0), tf.float32))
-    SS_res =  K.sum(K.square(y_t - y_p)) 
-    SS_tot = K.sum(K.square(y_t - K.mean(y_t))) 
-    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+    SS_res = K.sum(K.square(y_t - y_p))
+    SS_tot = K.sum(K.square(y_t - K.mean(y_t)))
+    return (1 - SS_res / (SS_tot + K.epsilon()))
 
 
-class data_generator( Sequence ) :
-  def __init__(self, xnames, ynames, batch_size) :
-    self.xnames = xnames
-    self.ynames = ynames
-    self.batch_size = batch_size
+class data_generator(Sequence):
+    def __init__(self, xnames, ynames, batch_size):
+        self.xnames = xnames
+        self.ynames = ynames
+        self.batch_size = batch_size
 
-  def __len__(self) :
-    return np.ceil( len(self.xnames) / float(self.batch_size)).astype(int)
-  
-  def __getitem__(self, idx) :
+    def __len__(self):
+        return np.ceil(len(self.xnames) / float(self.batch_size)).astype(int)
 
-    batch_x = self.xnames[ idx*self.batch_size : idx*self.batch_size + self.batch_size ]
-    batch_y = self.ynames[ idx*self.batch_size : idx*self.batch_size + self.batch_size ]
+    def __getitem__(self, idx):
+        batch_x = self.xnames[idx * self.batch_size: idx * self.batch_size + self.batch_size]
+        batch_y = self.ynames[idx * self.batch_size: idx * self.batch_size + self.batch_size]
 
+        d2m = np.stack([np.load(s)['d2m'] for s in batch_x])  # batch_size, 10, 56, 360
+        t2m = np.stack([np.load(s)['t2m'] for s in batch_x])  # batch_size, 10, 56, 360
+        skt = np.stack([np.load(s)['skt'] for s in batch_x])  # batch_size, 10, 56, 360
+        sp = np.stack([np.load(s)['sp'] for s in batch_x])  # batch_size, 10, 56, 360
+        sza = np.stack([np.load(s)['sza'] for s in batch_x])  # batch_size, 10, 56, 360
+        raa = np.stack([np.load(s)['raa'] for s in batch_x])  # batch_size, 10, 56, 360
+        o4 = np.stack([np.load(s)['o4'] for s in batch_x])  # batch_size, 10, 56, 360
 
-    d2m = np.stack([ np.load(s)['d2m'] for s in batch_x])   # batch_size, 10, 56, 360
-    t2m = np.stack([ np.load(s)['t2m'] for s in batch_x])   # batch_size, 10, 56, 360
-    skt = np.stack([ np.load(s)['skt'] for s in batch_x])   # batch_size, 10, 56, 360
-    sp = np.stack([ np.load(s)['sp'] for s in batch_x])   # batch_size, 10, 56, 360
-    sza = np.stack([ np.load(s)['sza'] for s in batch_x])   # batch_size, 10, 56, 360
-    raa = np.stack([ np.load(s)['raa'] for s in batch_x])   # batch_size, 10, 56, 360
-    o4 = np.stack([ np.load(s)['o4'] for s in batch_x])   # batch_size, 10, 56, 360
-    tempx1 = np.stack([d2m, t2m, skt, sp, sza, raa, o4], axis=-1)
-    tprof = np.stack([ np.load(s)['tprof'] for s in batch_x])   # batch_size, 10, 56, 360
-    qprof = np.stack([ np.load(s)['qprof'] for s in batch_x])   # batch_size, 10, 56, 360
-    uprof = np.stack([ np.load(s)['uprof'] for s in batch_x])   # batch_size, 10, 56, 360
-    vprof = np.stack([ np.load(s)['vprof'] for s in batch_x])   # batch_size, 10, 56, 360
-    tempx2 = np.stack([tprof, qprof, uprof, vprof], axis=-1)
+        tempx1 = np.stack([d2m, t2m, skt, sp, sza, raa, o4], axis=-1)
 
-    tempy = np.stack([ np.load(s)['aero'] for s in batch_y])[:, :, np.newaxis]   # batch, 
+        tprof = np.stack([np.load(s)['tprof'] for s in batch_x])  # batch_size, 10, 56, 360
+        qprof = np.stack([np.load(s)['qprof'] for s in batch_x])  # batch_size, 10, 56, 360
+        uprof = np.stack([np.load(s)['uprof'] for s in batch_x])  # batch_size, 10, 56, 360
+        vprof = np.stack([np.load(s)['vprof'] for s in batch_x])  # batch_size, 10, 56, 360
 
-    return [tempx1, tempx2], tempy
+        if AVK:
+            aeroavk = np.stack([np.load(s)['aeroavk'] for s in batch_x])
+        #     no2avk = np.stack([np.load(s)['no2avk'] for s in batch_x])
+
+        tempx2 = np.stack([tprof, qprof, uprof, vprof, aeroavk], axis=-1)
+
+        tempy = np.stack([np.load(s)['aero'] for s in batch_y])[:, :, np.newaxis]  # batch
+
+        return [tempx1, tempx2], tempy
+
 
 def data_split(x, y, ratio1, ratio2, maskname=None):
-    dsize1 = int(x.shape[0]*ratio1)
-    dsize2 = int(x.shape[0]*ratio2)
+    dsize1 = int(x.shape[0] * ratio1)
+    dsize2 = int(x.shape[0] * ratio2)
     dmask = np.array(list(range(0, x.shape[0])))
     random.shuffle(dmask)
 
@@ -95,49 +117,89 @@ def data_split(x, y, ratio1, ratio2, maskname=None):
 
     return x1, y1, x2, y2, x3, y3
 
+
 def get_x(fname):
-    d2m = np.load(fname)['d2m']    # batch_size, 10, 56, 360
-    t2m = np.load(fname)['t2m']    # batch_size, 10, 56, 360
-    skt = np.load(fname)['skt']    # batch_size, 10, 56, 360
-    sp = np.load(fname)['sp']    # batch_size, 10, 56, 360
-    sza = np.load(fname)['sza']    # batch_size, 10, 56, 360
-    raa = np.load(fname)['raa']    # batch_size, 10, 56, 360
-    o4 = np.load(fname)['o4']    # batch_size, 10, 56, 360
+    d2m = np.load(fname)['d2m']  # batch_size, 10, 56, 360
+    t2m = np.load(fname)['t2m']  # batch_size, 10, 56, 360
+    skt = np.load(fname)['skt']  # batch_size, 10, 56, 360
+    sp = np.load(fname)['sp']  # batch_size, 10, 56, 360
+    sza = np.load(fname)['sza']  # batch_size, 10, 56, 360
+    raa = np.load(fname)['raa']  # batch_size, 10, 56, 360
+    o4 = np.load(fname)['o4']  # batch_size, 10, 56, 360
+
     tempx1 = np.stack([d2m, t2m, skt, sp, sza, raa, o4], axis=-1)
     tempx1 = tempx1[np.newaxis, :, :]
-    tprof = np.load(fname)['tprof']    # batch_size, 10, 56, 360
-    qprof = np.load(fname)['qprof']    # batch_size, 10, 56, 360
-    uprof = np.load(fname)['uprof']    # batch_size, 10, 56, 360
-    vprof = np.load(fname)['vprof']    # batch_size, 10, 56, 360
-    tempx2 = np.stack([tprof, qprof, uprof, vprof], axis=-1)
+
+    tprof = np.load(fname)['tprof']  # batch_size, 10, 56, 360
+    qprof = np.load(fname)['qprof']  # batch_size, 10, 56, 360
+    uprof = np.load(fname)['uprof']  # batch_size, 10, 56, 360
+    vprof = np.load(fname)['vprof']  # batch_size, 10, 56, 360
+
+    if AVK:
+        aeroavk = np.load(fname)['aeroavk']
+    #     no2avk = np.load(fname)['no2avk']
+
+    tempx2 = np.stack([tprof, qprof, uprof, vprof, aeroavk], axis=-1)
     tempx2 = tempx2[np.newaxis, :, :]
+
     return [tempx1, tempx2]
 
-# seed(6658)
+
+def outname(inname):
+    return 'aeropred' + inname.split('/')[-1][1:-1] + 'y'
 
 
+## Input data preparation
 
-xfiles = np.array(sorted(glob.glob('X/X_*.npz')))
-yfiles = np.array(sorted(glob.glob('Y/Y_*.npz')))
+preset_mask = False  # wether to load previous mask
 
-xlist1, ylist1, xlist2, ylist2, xlist3, ylist3 = data_split(xfiles, yfiles, 0.7225, 0.85, maskname='train_valid_test_mask.npz')
+if FLAG == 0:
+    xfiles = np.array(sorted(glob.glob(INPATH + 'X/X_*.npz')))
+    yfiles = np.array(sorted(glob.glob(INPATH + 'Y/Y_*.npz')))
 
-mask1 = np.load('train_valid_test_mask.npz')['train']
-mask2 = np.load('train_valid_test_mask.npz')['valid']
-mask3 = np.load('train_valid_test_mask.npz')['test']
-xlist1 = xfiles[mask1]
-ylist1 = yfiles[mask1]
-xlist2 = xfiles[mask2]
-ylist2 = yfiles[mask2]
-xlist3 = xfiles[mask3]
-ylist3 = yfiles[mask3]
+else:
+    xfiles = np.array(sorted(glob.glob(INPATH + 'X_flag%s/X_*.npz' % FLAG)))
+    yfiles = np.array(sorted(glob.glob(INPATH + 'Y_flag%s/Y_*.npz' % FLAG)))
 
-print('Splitting: ', len(xlist1), len(ylist1), len(xlist2), len(ylist2), len(xlist3), len(ylist3) )
-train_generator = data_generator(xlist1, ylist1, 5)   # 90%*900 = 810
-valid_generator = data_generator(xlist2, ylist2, 5)   # 10%*900 = 90
+if preset_mask:
+
+    maskfile = "../input/pandora/train_valid_test_mask.npz"
+
+    mask1 = np.load(maskfile)['train']
+    mask2 = np.load(maskfile)['valid']
+    mask3 = np.load(maskfile)['test']
+
+    xlist1 = xfiles[mask1]
+    ylist1 = yfiles[mask1]
+    xlist2 = xfiles[mask2]
+    ylist2 = yfiles[mask2]
+    xlist3 = xfiles[mask3]
+    ylist3 = yfiles[mask3]
+
+else:
+    xlist1, ylist1, \
+    xlist2, ylist2, \
+    xlist3, ylist3 = data_split(xfiles, yfiles, SPLIT[0], SPLIT[1],
+                                maskname='train_valid_test_mask.npz')
+
+print('Splitting: ',
+      len(xlist1), len(ylist1),
+      len(xlist2), len(ylist2),
+      len(xlist3), len(ylist3))
+
+train_generator = data_generator(xlist1, ylist1, BCHSIZE)
+valid_generator = data_generator(xlist2, ylist2, BCHSIZE)
+test_generator = data_generator(xlist3, ylist3, BCHSIZE)
 
 
-prof_input = Input((21, 4), name='rean_input')
+## Model setup
+
+if AVK:
+    input_size = (21, 5)
+else:
+    input_size = (21, 4)
+
+prof_input = Input((input_size), name='rean_input')
 meas_input = Input((9, 7), name='meas_input')
 
 pc1 = Conv1D(36, 2, strides=1, padding='same', activation="relu")(meas_input)
@@ -151,7 +213,7 @@ pc3 = Conv1D(256, 2, strides=1, padding='same', activation="relu")(pc3)
 pd3 = Dense(256, activation="relu")(pc3) # 21
 
 lstm = LSTM(21, name='LSTM1') (pd3)  # this gives a pseudo profile
-lstm = kl.Reshape( (21, 1)) (lstm)
+lstm = kl.Reshape((21, 1)) (lstm)
 
 concat_layer = concatenate([prof_input, lstm])   # 21, 5
 
@@ -159,12 +221,12 @@ c1 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(concat_layer) 
 c1 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(c1)  # 21, 32
 p1 = Dense(128, activation="relu") (c1)   # 21, 32
 
-c2 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(p1)  # 21, 64 
-c2 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(c2)          # 21, 64
+c2 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(p1)  # 21, 64
+c2 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(c2)  # 21, 64
 p2 = Dense(128, activation="relu") (c2)   # 21, 64
 
 c3 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(p2) # 21, 128
-c3 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(c3)         # 21, 128
+c3 = Conv1D(128, 2, strides=1, padding='same', activation="relu")(c3) # 21, 128
 p3 = Dense(128, activation="relu") (c3)   # 21, 128
 
 u = concatenate([p1, p2, p3])   # 21, 5
@@ -175,25 +237,69 @@ output = Dense(1, activation="linear")(u) # 1
 model = Model(inputs=[meas_input, prof_input], outputs=output)
 
 
-opt = Adam(lr=1e-5)
+opt = Adam(learning_rate = LR)
 model.compile(optimizer=opt, loss='mse', metrics=[r2_keras, 'mse'])
 model.summary()
-model.load_weights('DLMAXDOAS_final.h5')
 
-csv_logger = CSVLogger( 'DLMAXDOAS_log.csv', append=True, separator=';')
+csv_logger = CSVLogger('DLMAXDOAS_log.csv', append=True, separator=';')
 earlystopper = EarlyStopping(patience=20, verbose=1)
-checkpointer = ModelCheckpoint('DLMAXDOAS_checkpt_{val_loss:.2f}.h5', verbose=1, save_best_only=True)
+checkpointer = ModelCheckpoint('DLMAXDOAS_checkpt_{val_loss:.2f}.h5',
+                               verbose=1, save_best_only=True)
+
+# Load weights from pre-trained model
+ep_complete = 0
+load_weights = False
+
+if load_weights:
+    model_path = "../input/pandora/saved_models/DLMAXDOAS_flag%s_ep%s.h5" % (FLAG, ep_complete)
+    model.load_weights(model_path)
+
+    print("Evaluation of pre-trained model for train set:")
+    print(model.evaluate(train_generator))
+
+    print("Evaluation of pre-trained model for val set:")
+    print(model.evaluate(valid_generator))
+
+    print("Evaluation of pre-trained model for test set:")
+    print(model.evaluate(test_generator))
+
+## Model fitting
+
 results = model.fit(train_generator,
-                    validation_data=valid_generator, epochs=500,
+                    validation_data=valid_generator, epochs=EPOCH,
                     callbacks=[earlystopper, checkpointer, csv_logger])
-model.save('DLMAXDOAS_final.h5')
+model.save('DLMAXDOAS_flag%s_ep%s.h5' % (FLAG, ep_complete+EPOCH))
 
+print("Evaluation of trained model for train set:")
+print(model.evaluate(train_generator))
 
-def outname(inname):
-	return 'Ypred' + inname.split('/')[-1][1:-1] + 'y'
+print("Evaluation of trained model for val set:")
+print(model.evaluate(valid_generator))
 
-flist = np.array(sorted(glob.glob('X/X_*.npz')))
+print("Evaluation of trained model for test set:")
+print(model.evaluate(test_generator))
 
-for file in flist:
-	ypred = model.predict( get_x(file) )[0]
-	np.save('Y_pred/' + outname(file), ypred)
+## Save outputs
+
+SaveOutput = False
+
+if SaveOutput:
+    output_train = OUTPATH + "train/"
+    output_val = OUTPATH + "val/"
+    output_test = OUTPATH + "test/"
+    os.mkdir(output_train)
+    os.mkdir(output_val)
+    os.mkdir(output_test)
+
+    for file in xlist1:
+        ypred = model.predict(get_x(file))[0]
+        np.save(os.path.join(output_train, outname(file)), ypred)
+
+    for file in xlist2:
+        ypred = model.predict(get_x(file))[0]
+        np.save(os.path.join(output_val, outname(file)), ypred)
+
+    for file in xlist3:
+        ypred = model.predict(get_x(file))[0]
+        np.save(os.path.join(output_test, outname(file)), ypred)
+
--- process_data.py
+++ process_data.py
@@ -1,3 +1,4 @@
+import os
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,34 +8,110 @@ from metpy.units import units
 import numpy as np
 from datetime import datetime, timedelta
 
+CMD = os.getcwd()
+DATAPATH = CMD + "/data/"  # raw retrieval + meteorological data
+INPATH = CMD + "/input/"  # input to ML model
+OUTPATH = CMD + "/output/"  # output from ML model
+AEROPATH = CMD + "/data/aerosol/"
+NO2PATH = CMD + "/data/no2/"
+METPATH = CMD + "/data/met/"
+
+chisq0 = 40  # chisq threshold
+dof0 = 1.5  # dof threshold
+
+
 def dt_from_filename(fname):
     dtstr = fname.split('.')[0][-13:]
     dtstr = datetime.strptime(dtstr, '%Y%m%d_%H%M')
     return dtstr
 
+
 def find_tidx(tarray, targ):
     curr = 0
-    while tarray[curr] + timedelta(hours=1)< targ:
+    while tarray[curr] + timedelta(hours=1) < targ:
         curr += 1
     return curr
 
+
 def to_meas(dt):
-    return 'For_Tailong/p103_short_uv_aer/%s/general/meas_%s.dat'%(dt.strftime('%Y%m%d'), dt.strftime('%Y%m%d'))
+    return AEROPATH + ('p103_short_uv/%s/general/meas_%s.dat'
+                       % (dt.strftime('%Y%m%d'), dt.strftime('%Y%m%d')))
 
 
+def aero_file(iid, type='profiles'):
+    date = iid.split('_')[0]
+    if 'prof' in type:
+        filename = 'prof361nm_' + iid + '.dat'
+    elif 'av' in type:
+        filename = 'avk_ext_' + iid + '.dat'
 
-flist = glob.glob('For_Tailong/p103_short_uv_tg/20180203/profiles/*dat')
-final_alt = np.arange(0, 4.2, 0.2)*1e3
+    return AEROPATH + 'p103_short_uv/%s/%s/' % (date, type) + filename
+
+
+def no2_file(iid, type='profiles'):
+    date = iid.split('_')[0]
+    if 'prof' in type:
+        filename = 'NO2_prof_' + iid + '.dat'
+    elif 'av' in type:
+        filename = 'avk_' + iid + '.dat'
+    return NO2PATH + 'p103_short_uv/%s/%s/' % (date, type) + filename
+
+
+def get_indicators(iid):
+    date = iid.split('_')[0]
+    filepath = AEROPATH + 'p103_short_uv/%s/' % date + 'retrieval_details/'
+    filename = 'retr_' + iid + '.dat'
+
+    df = pd.read_csv(os.path.join(filepath, filename), sep=':',
+                     encoding="ISO-8859-1",
+                     index_col=False, names=['Info', 'Value'])
+    assert df.loc[1, 'Info'] == 'Chisquare'
+    chisq = df.loc[1, 'Value']
+
+    assert df.loc[3, 'Info'] == "Degrees of freedom for signal"
+    dof = df.loc[3, 'Value']
+
+    return chisq, dof
+
+
+flist = glob.glob(NO2PATH + 'p103_short_uv/*/profiles/*dat')
+final_alt = np.arange(0, 4.2, 0.2) * 1e3
 
 ref = datetime(2018, 2, 2)
+o4_nan = 0
+no2_nan = 0
+aero_nan = 0
+missing_aero = 0
+missing_meas = 0
+missing_retr = 0
+notfound = 0
+exclude = 0
 
 for f in flist:
-    dtnow = dt_from_filename(f) 
+
+    iid = f.split('/')[-1][-17:-4]
+
+    try:
+        chisq, dof = get_indicators(iid)
+
+    except Exception as e:
+        print(e)
+        missing_retr += 1
+        continue
+
+    dtnow = dt_from_filename(f)
 
     df = pd.read_csv(to_meas(dtnow), delim_whitespace=True)
     dts = df['date'] + ' ' + df['time']
     dts = np.array([datetime.strptime(x, '%d/%m/%Y %H:%M:%S') for x in dts])
-    dts = dts.reshape(-1, 9)
+
+    try:
+        dts = dts.reshape(-1, 9)
+    except Exception as e:
+        print(e)
+        missing_meas += 1
+        continue
+
     meandts = np.max(dts, axis=-1)
     meastidx = -1
     found = False
@@ -42,32 +119,35 @@ for f in flist:
         if meandts[i] <= dtnow:
             meastidx = i
             found = True
-    if found: 
+
+    if found:
         sza = df['SZA'].to_numpy().reshape(-1, 9)
         raa = df['rel_azim'].to_numpy().reshape(-1, 9)
         o4 = df['O4meas'].to_numpy().reshape(-1, 9)
 
-        sza = sza[meastidx]/1e2
-        raa = raa[meastidx]/1e2
-        o4 = o4[meastidx]*1e2
+        sza = sza[meastidx] / 1e2
+        raa = raa[meastidx] / 1e2
+        o4 = o4[meastidx] * 1e2
 
-        fh = nc.Dataset('ERA5_preslevels.nc')
+        iid = f.split('/')[-1][-17:-11]
+
+        fh = nc.Dataset(METPATH + 'ERA5_preslevels_%s.nc' % (iid))
         eralons = fh.variables['longitude'][:]
         eralats = fh.variables['latitude'][:]
-        jidx = np.argmin(abs(eralats - 43.7267 ))
-        iidx = np.argmin(abs(eralons - -79.4821 ))
+        jidx = np.argmin(abs(eralats - 43.7267))
+        iidx = np.argmin(abs(eralons - -79.4821))
         times = np.array(fh.variables['time'][:])
-        times = times*timedelta(hours=1) + datetime(1900, 1, 1, 0, 0)
+        times = times * timedelta(hours=1) + datetime(1900, 1, 1, 0, 0)
         tidx = find_tidx(times, dtnow)
 
         geop = fh.variables['z'][tidx, ::-1, jidx, iidx]
-        tprof = fh.variables['t'][tidx, ::-1, jidx, iidx]/100
-        qprof = fh.variables['q'][tidx, ::-1, jidx, iidx]*1e3   # g kg**-1
-        uprof = fh.variables['u'][tidx, ::-1, jidx, iidx]/10
-        vprof = fh.variables['v'][tidx, ::-1, jidx, iidx]/10
+        tprof = fh.variables['t'][tidx, ::-1, jidx, iidx] / 100
+        qprof = fh.variables['q'][tidx, ::-1, jidx, iidx] * 1e3  # g kg**-1
+        uprof = fh.variables['u'][tidx, ::-1, jidx, iidx] / 10
+        vprof = fh.variables['v'][tidx, ::-1, jidx, iidx] / 10
         geop = units.Quantity(geop, "m**2 s**-2")
         height = metpy.calc.geopotential_to_height(geop)
-        height = np.array(height) - 76   # downsview's altitude above mean sea level is 76 m.
+        height = np.array(height) - 76  # Downsview's altitude above mean sea level is 76 m.
         fh.close()
 
         tprof = np.interp(final_alt, height, tprof)
@@ -75,43 +155,124 @@ for f in flist:
         uprof = np.interp(final_alt, height, uprof)
         vprof = np.interp(final_alt, height, vprof)
 
-
-        fh = nc.Dataset('ERA5_singlelevels.nc')
+        fh = nc.Dataset(METPATH + 'ERA5_singlelevels_2018.nc')
         eralons = fh.variables['longitude'][:]
         eralats = fh.variables['latitude'][:]
-        jidx = np.argmin(abs(eralats - 43.7267 ))
-        iidx = np.argmin(abs(eralons - -79.4821 ))
+        jidx = np.argmin(abs(eralats - 43.7267))  # Downsview's coordinate 43.7267° N, 79.4821° W
+        iidx = np.argmin(abs(eralons - -79.4821))
         times = np.array(fh.variables['time'][:])
-        times = times*timedelta(hours=1) + datetime(1900, 1, 1, 0, 0)
+        times = times * timedelta(hours=1) + datetime(1900, 1, 1, 0, 0)
         tidx = find_tidx(times, dtnow)
-        d2m = fh.variables['d2m'][tidx, jidx, iidx]/100
-        t2m = fh.variables['t2m'][tidx, jidx, iidx]/100
-        skt = fh.variables['skt'][tidx, jidx, iidx]/100
-        sp = fh.variables['sp'][tidx, jidx, iidx]/1e5  # bar
+        d2m = fh.variables['d2m'][tidx, jidx, iidx] / 100
+        t2m = fh.variables['t2m'][tidx, jidx, iidx] / 100
+        skt = fh.variables['skt'][tidx, jidx, iidx] / 100
+        sp = fh.variables['sp'][tidx, jidx, iidx] / 1e5  # bar
         fh.close()
 
-        
-        iid = f.split('\\')[-1][-17:-4]
+        iid = f.split('/')[-1][-17:-4]
         try:
-            aerodf = pd.read_csv(aero_file(iid), delim_whitespace=True)
+            aerodf = pd.read_csv(aero_file(iid, 'profiles'), delim_whitespace=True)
+            aeroavk_df = pd.read_csv(aero_file(iid, 'av_kernels'), delim_whitespace=True)
+            no2avk_df = pd.read_csv(no2_file(iid, 'av_kernels'), delim_whitespace=True)
         except Exception as e:
             print(e)
-            pass
+            missing_aero += 1
+            continue
 
         no2df = pd.read_csv(f, delim_whitespace=True)
         no2prof = no2df['retr_nd']
         no2err = no2df['err_r_nd']
+        no2avk = no2avk_df['area']
+
         aeroprof = aerodf['retrieved']
         aeroerr = aerodf['err_retrieved']
-        
-        d2m = np.ones(9)*d2m
-        t2m = np.ones(9)*d2m
-        skt = np.ones(9)*d2m
-        sp = np.ones(9)*d2m
-        #print(np.mean(d2m), np.mean(t2m), np.mean(skt), np.mean(sp), np.mean(sza), np.mean(raa), np.mean(o4), np.mean(tprof), np.mean(qprof), np.mean(uprof), np.mean(vprof))
+        aeroavk = aeroavk_df['area']
+
+        d2m = np.ones(9) * d2m
+        t2m = np.ones(9) * t2m
+        skt = np.ones(9) * skt
+        sp = np.ones(9) * sp
+        # print(np.mean(d2m), np.mean(t2m), np.mean(skt), np.mean(sp),
+        # np.mean(sza), np.mean(raa), np.mean(o4), np.mean(tprof),
+        # np.mean(qprof), np.mean(uprof), np.mean(vprof))
+
         print(dtnow, times[tidx], meandts[meastidx])
-        np.savez('X/X_%s.npz'%(dtnow.strftime('%Y%m%d_%H%M%S')), d2m=d2m, t2m=t2m, skt=skt, 
-                 sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof, qprof=qprof, uprof=uprof, vprof=vprof)
-        np.savez('Y/Y_%s.npz'%(dtnow.strftime('%Y%m%d_%H%M%S')), aero=aeroprof, no2=no2prof/1e11)
+
+        if np.any(np.isnan(o4)):
+            o4_nan += 1
+            print("O4 measurement data contains Nan!")
+            continue
+
+        if np.any(np.isnan(aeroprof)):
+            aero_nan += 1
+            print("Aero profile data contains Nan!")
+            continue
+
+        if np.any(np.isnan(no2prof)):
+            no2_nan += 1
+            print("NO2 profile data contains Nan!")
+            continue
+
+        xpath = INPATH + 'X/'
+        ypath = INPATH + 'Y/'
+        np.savez(os.path.join(xpath, 'X_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                 d2m=d2m, t2m=t2m, skt=skt, sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof,
+                 qprof=qprof, uprof=uprof, vprof=vprof, aeroavk=aeroavk, no2avk=no2avk)
+        np.savez(os.path.join(ypath, 'Y_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                 aero=aeroprof, no2=no2prof / 1e11, chisq=chisq, dof=dof)
+
+        if dof > dof0 and chisq < chisq0:
+
+            xpath = INPATH + 'X_flag1/'
+            ypath = INPATH + 'Y_flag1/'
+            np.savez(os.path.join(xpath, 'X_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     d2m=d2m, t2m=t2m, skt=skt, sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof,
+                     qprof=qprof, uprof=uprof, vprof=vprof, aeroavk=aeroavk, no2avk=no2avk)
+            np.savez(os.path.join(ypath, 'Y_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     aero=aeroprof, no2=no2prof / 1e11, chisq=chisq, dof=dof)
+
+        elif dof < dof0 and chisq > chisq0:
+
+            xpath = INPATH + 'X_flag2/'
+            ypath = INPATH + 'Y_flag2/'
+            np.savez(os.path.join(xpath, 'X_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     d2m=d2m, t2m=t2m, skt=skt, sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof,
+                     qprof=qprof, uprof=uprof, vprof=vprof, aeroavk=aeroavk, no2avk=no2avk)
+            np.savez(os.path.join(ypath, 'Y_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     aero=aeroprof, no2=no2prof / 1e11, chisq=chisq, dof=dof)
+
+        elif dof > dof0 and chisq > chisq0:
+
+            xpath = INPATH + 'X_flag3/'
+            ypath = INPATH + 'Y_flag3/'
+            np.savez(os.path.join(xpath, 'X_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     d2m=d2m, t2m=t2m, skt=skt, sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof,
+                     qprof=qprof, uprof=uprof, vprof=vprof, aeroavk=aeroavk, no2avk=no2avk)
+            np.savez(os.path.join(ypath, 'Y_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     aero=aeroprof, no2=no2prof / 1e11, chisq=chisq, dof=dof)
+
+        elif dof < dof0 and chisq < chisq0:
+
+            xpath = INPATH + 'X_flag4/'
+            ypath = INPATH + 'Y_flag4/'
+            np.savez(os.path.join(xpath, 'X_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     d2m=d2m, t2m=t2m, skt=skt, sp=sp, sza=sza, raa=raa, o4=o4, tprof=tprof,
+                     qprof=qprof, uprof=uprof, vprof=vprof, aeroavk=aeroavk, no2avk=no2avk)
+            np.savez(os.path.join(ypath, 'Y_%s.npz' % (dtnow.strftime('%Y%m%d_%H%M%S'))),
+                     aero=aeroprof, no2=no2prof / 1e11, chisq=chisq, dof=dof)
+
+        else:
+            pass
+
     else:
         print('NOT FOUND!')
+        notfound += 1
+
+print("Number of invalid O4 data: %s" % o4_nan)
+print("Number of invalid NO2 data: %s" % no2_nan)
+print("Number of invalid Aerosol data: %s" % aero_nan)
+print("Number of missing Aerosol profiles: %s" % missing_aero)
+print("Number of missing Aerosol retrieval info: %s" % missing_retr)
+print("Number of missing Elevation measurement: %s" % missing_meas)
+print("Number of Not Found: %s" % notfound)
+print("Number of excluded data: %s" % exclude)
